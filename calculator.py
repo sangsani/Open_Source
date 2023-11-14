88eaa6367ff0ee5bbf52ce4bd8f7c38b0fec18bb
sangsani <125367710+sangsani@users.noreply.github.com>
2023-11-14 14:35:04 +0900

    Add comments calculator.py
    Add comments to pull func test in calculator.py

--- calculator.py
+++ calculator.py
@@ -1,5 +1,10 @@
 # My Calculator
-def add(a,b);
-    return a+b;
-def sub(a,b);
-    return a-b;
+
+def add(a,b):
+    return a+b
+    
+def sub(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
